996f34e6f4e6ca1fa06747b335b4e73a7315a23b
Tom Higgins <tomhiggins@gmail.com>
2014-05-21 22:51:58 -0600

    cleaning up the root directory

--- pirateboxPi/piratebox/bin/station_cnt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-CNT=`iw wlan0 station dump | grep Station | wc -l`
-#DATE=`date`
-#echo $DATE - $CNT
-echo "<a title='Counter is refreshed every 2 minutes, you need to refresh the page'>Currently there are $CNT connected clients</a>"
--- voice/readme.txt
+++
@@ -1,2 +0,0 @@
-Test if festivle/flite is doable on the pi. If so hook into  the framework
-
