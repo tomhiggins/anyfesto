255dba12153a4eee0646f129c00ce1fd64437133
tomhiggins <tomhiggins@gmail.com>
2015-12-09 18:26:02 -0700

    updating deployables for working rev 2

--- deployables/opt/qwebirc/qwebirc/config_options.py
+++
@@ -1,9 +0,0 @@
-IDENT_HEX = object()
-IDENT_NICKNAME = object()
-WEBIRC_REALNAME = object()
-
-def get(name, default=None):
-  import config
-  if hasattr(config, name):
-    return getattr(config, name)
-  return default
--- deployables/opt/qwebirc/qwebirc/util/__init__.py
+++
@@ -1,1 +0,0 @@
-from hitcounter import HitCounter
