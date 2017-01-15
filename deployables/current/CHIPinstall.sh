607051ac5d8336a71fe57ef514e736cc6a5b60a0
Tom Higgins <tomhiggins@gmail.com>
2017-01-15 04:41:00 -0700

    Install Script update for Winter Code Cleanup

--- deployables/current/CHIPinstall.sh
+++ deployables/current/CHIPinstall.sh
@@ -1,66 +1,86 @@
 #!/bin/bash
 
 # Anyfesto Install Script for the CHIP
-# CC 2016 by tomwsmf
+# CC 2017 by tomwsmf
 
-# Upgrae your base install
+# Install the Basic Packages and Infrastructure
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install locales
 sudo locale-gen en_US en_US.UTF-8
-
-# Start installing the services
 sudo apt-get -y install vlc
 sudo adduser --no-create-home --shell /bin/false --disabled-password vlc
 sudo usermod -G audio,chip vlc
 sudo apt-get -y install lighttpd
-sudo /etc/init.d/lighttpd stop
-sudo update-rc.d lighttpd remove
-sudo apt-get -y install dnsmasq 
-sudo /etc/init.d/dnsmasq  stop
-sudo update-rc.d dnsmasq remove 
-sudo apt-get -y  install hostapd
-sudo /etc/init.d/hostapd  stop
-sudo update-rc.d hostapd remove 
+sudo apt-get -y install dnsmasq
+sudo apt-get -y install isc-dhcp-server
+sudo apt-get -y install hostapd
 sudo apt-get -y install git
 sudo apt-get -y install perl sox libsox-fmt-all libav-tools
 sudo rm /bin/sh
 sudo ln /bin/bash /bin/sh
 sudo chmod a+rw /bin/sh
+
+# Setup the Directories and lighttpd 
 mkdir /home/chip/content
 mkdir /home/chip/content/Radio
 sudo usermod -d /home/chip/content/Radio vlc
-
-# Install and Config Piratebox Base
+sudo ln -s /home/chip/content /var/www/html/content   
+cd /var/www/html
+sudo rm index*.html
+sudo wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/AnyfestoCHiP-index.html -O index.html
+sudo wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/anyfestochiplogosm.jpg -O logo.jpg
 cd ~
-wget  http://downloads.piratebox.de/piratebox-ws_current.tar.gz
-tar xzf piratebox-ws_current.tar.gz
-cd piratebox
-sudo mkdir -p  /opt
-sudo cp -rv  piratebox /opt
-cd /opt/piratebox
-sudo sed 's:DROOPY_USE_USER="no":DROOPY_USE_USER="yes":' -i  /opt/piratebox/conf/piratebox.conf
-sudo ln -s /opt/piratebox/init.d/piratebox /etc/init.d/piratebox
-sudo update-rc.d piratebox  defaults 
-sudo ln -s /home/chip/content /opt/piratebox/share
+wget https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip
+unzip bootstrap-3.3.7-dist.zip 
+sudo cp -r bootstrap-3.3.7-dist/* /var/www/html/
+cd /var/www/html/css
+sudo wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/carousel.css
+cd /var/www/html/js
+sudo wget https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+cd ~
+sudo echo 'server.dir-listing = "enable"'  >> /etc/lighttpd/lighttpd.conf
 
-# Change the defaults in /opt/piratebox/conf/piratebox.conf   
-sudo sed 's:HOST="piratebox.lan":HOST="anyfestochip.lan":' -i /opt/piratebox/conf/piratebox.conf
-sudo sed 's:INTERFACE="wlan0":INTERFACE="wlan1":' -i /opt/piratebox/conf/piratebox.conf
-sudo sed 's:DNSMASQ_INTERFACE="wlan0":DNSMASQ_INTERFACE="wlan1":' -i /opt/piratebox/conf/piratebox.conf
-sudo sed 's:NET=192.168.77:NET=10.11.99:' -i /opt/piratebox/conf/piratebox.conf
-      
-# Setup hostapd configs in /opt/piratebox/conf/hostapd.conf       
-sudo echo "interface=wlan1" >/opt/piratebox/conf/hostapd.conf
-sudo echo "driver=nl80211" >>/opt/piratebox/conf/hostapd.conf
-sudo echo "ssid=AnyfestoCHIPCommunityNetworking" >>/opt/piratebox/conf/hostapd.conf
-sudo echo "channel=1" >>/opt/piratebox/conf/hostapd.conf
-sudo echo "ctrl_interface=/var/run/hostapd" >>/opt/piratebox/conf/hostapd.conf
-      
-# Setup hostapd configs in /etc/default/hostapd                  
-sudo echo 'DAEMON_CONF="/opt/piratebox/conf/hostapd.conf"' >/etc/default/hostapd
+#
+#Setup Network and Captive Portal 
+#
 
-# Setup allowed hosts for vlc servers in  /usr/share/vlc/lua/http/.hosts
+sudo echo 'INTERFACES="wlan1"' >/etc/default/isc-dhcp-server
+
+sudo echo "ddns-update-style none;" >/etc/dhcp/dhcpd.conf
+sudo echo "log-facility local7;" >>/etc/dhcp/dhcpd.conf
+sudo echo "authoritative;" >>/etc/dhcp/dhcpd.conf
+sudo echo "subnet 10.11.99.0 netmask 255.255.255.0 {" >>/etc/dhcp/dhcpd.conf
+sudo echo "range 10.11.99.20 10.11.99.200;" >>/etc/dhcp/dhcpd.conf
+sudo echo "option broadcast-address 10.11.99.255;" >>/etc/dhcp/dhcpd.conf
+sudo echo "option routers 10.11.99.1;" >>/etc/dhcp/dhcpd.conf
+sudo echo "default-lease-time 600;" >>/etc/dhcp/dhcpd.conf
+sudo echo "max-lease-time 7200;" >>/etc/dhcp/dhcpd.conf
+sudo echo "option domain-name "local";" >>/etc/dhcp/dhcpd.conf
+sudo echo "option domain-name-servers 10.11.99.1, 10.11.99.1;" >>/etc/dhcp/dhcpd.conf
+sudo echo "}" >>/etc/dhcp/dhcpd.conf
+sudo echo " " >>/etc/dhcp/dhcpd.conf
+
+sudo echo "address=/#/10.11.99.1" >/etc/dnsmasq.conf
+sudo echo "interface=wlan1" >>/etc/dnsmasq.conf
+sudo echo "source-directory /etc/network/interfaces.d" > /etc/network/interfaces 
+sudo echo "auto wlan1" >> /etc/network/interfaces 
+sudo echo "iface wlan1 inet static" >> /etc/network/interfaces 
+sudo echo "address 10.11.99.1" >> /etc/network/interfaces 
+sudo echo "netmask 255.255.255.0" >> /etc/network/interfaces 
+sudo echo "gateway 10.11.99.1" >> /etc/network/interfaces 
+
+sudo echo "interface=wlan1" >/etc/hostapd/hostapd.conf 
+sudo echo "driver=nl80211" >>/etc/hostapd/hostapd.conf 
+sudo echo "ssid=AnyfestoCHIPCommunityNetworking" >>/etc/hostapd/hostapd.conf 
+sudo echo "channel=1" >>/etc/hostapd/hostapd.conf 
+sudo echo "ctrl_interface=/var/run/hostapd" >>/etc/hostapd/hostapd.conf 
+
+sudo echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >/etc/default/hostapd
+
+sudo echo "AnyfestoCHIP" >/etc/hostname 
+
+#Setup Audio Streaming using VLC
 sudo echo "::1" > /usr/share/vlc/lua/http/.hosts
 sudo echo "127.0.0.1" >> /usr/share/vlc/lua/http/.hosts
 sudo echo "fc00::/7" >> /usr/share/vlc/lua/http/.hosts
@@ -68,7 +88,6 @@ sudo echo "fec0::/10" >> /usr/share/vlc/lua/http/.hosts
 sudo echo "10.0.0.0/8" >> /usr/share/vlc/lua/http/.hosts
 sudo echo "192.0.0.0/8" >> /usr/share/vlc/lua/http/.hosts
 
-# Setuo vlc server and put in a default file for streaming
 sudo mkdir /etc/vlc 
 cd /usr/share/vlc/lua/http/ 
 sudo mv .hosts /etc/vlc 
@@ -88,18 +107,6 @@ sudo echo "sudo -u vlc cvlc -vvv -I http --http-password $VLC_PASWD  --http-host
 sudo chmod a+rx /etc/vlc/start.sh  
 sudo sed 's:exit 0:/etc/vlc/start.sh \&\nexit 0:' -i /etc/rc.local
 
-
-
-# Get default web pages and clean up the piratebox install
-sudo rm /opt/piratebox/www/index.html
-sudo wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/AnyfestoCHiP-index.html -O /opt/piratebox/www/index.html
-sudo wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/anyfestochiplogosm.jpg -O /opt/piratebox/www/logo.jpg
-sudo rm /opt/piratebox/www/favicon.ico
-sudo rm /opt/piratebox/www/Shared/HEADER.txt
-sudo rm /opt/piratebox/www/Shared/README.txt
-
-sudo /etc/init.d/piratebox start
-
 sudo sync 
 sudo reboot
-
+#
