590a569dfb794d10362f08067f84a6b6d7564b10
Tom Higgins <tomhiggins@gmail.com>
2017-01-19 01:57:53 -0700

    Added ircd and kiwiirc to the basic install
    Verified and working ircd and kiwiirc web front end.

--- deployables/current/CHIPinstall.sh
+++ deployables/current/CHIPinstall.sh
@@ -97,6 +97,32 @@ wget https://archive.org/download/Old_Radio_Public_Service_Announcements/OldRadi
 sudo mv -f ~/configs/start.sh /etc/vlc/start.sh  
 sudo chmod a+rx /etc/vlc/start.sh
 
+#Setup Text Chat ircd-hybrid and kiwiirc web 
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+echo "Setting Up the Text Chat Service
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+sudo apt-get install ircd-hybrid
+cd ~
+cd configs
+wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/current/ircd.conf
+wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/current/ircd.motd
+sudo chown root:root *
+sudo chmod a+rx *
+sudo mv -f ircd.conf /etc/ircd-hybrid/ 
+sudo mv -f ircd.motd /etc/ircd-hybrid/ircd.motd   
+sudo apt-get install npm
+cd ~
+git clone https://github.com/prawnsalad/KiwiIRC.git
+cd KiwiIRC
+npm install
+wget https://raw.githubusercontent.com/tomhiggins/anyfesto/master/deployables/current/kiwiconfig.js
+cp kiwiconfig.js config.js  
+sudo chmod a+rx config.js
+./kiwi build
+./kiwi start
+cd ~
+
+sudo update-rc.d ircd-hybrid enable
 sudo update-rc.d hostapd enable
 sudo update-rc.d isc-dhcp-server enable 
 
