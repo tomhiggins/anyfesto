b7556df781372544edaf1cb7edcca0551eef39c9
Tom Higgins <tomhiggins@gmail.com>
2017-01-25 22:19:54 -0700

    Update start.sh

--- deployables/current/start.sh
+++ deployables/current/start.sh
@@ -6,4 +6,3 @@ VLC_USER=vlc
 VLC_IP=10.11.99.1
 VLC_PASWD=changeme
 sudo -u vlc cvlc -vvv -I http --http-password $VLC_PASWD --http-host $VLC_IP --http-port $VLC_PORT --sout-keep --sout='#duplicate{dst=rtp{mux=ts,dst=10.11.99.1:8086},dst=gather:std{access=http,mux=mpeg1,dst=:8085},dst=display,select="novideo"}' -LZ /home/chip/content/*/./*.mp3
-/home/chip/KiwiIRC/kiwi start
