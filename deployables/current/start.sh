b64e3f64d685c04bb39f12465e0a855cffc3b5cb
Tom Higgins <tomhiggins@gmail.com>
2017-07-12 04:38:52 -0600

    Move to a playlist setup
    Will be update on each reboot or by hand by running /home/chip/makeplaylist.sh

--- deployables/current/start.sh
+++ deployables/current/start.sh
@@ -3,4 +3,4 @@ VLC_PORT=8088
 VLC_USER=vlc
 VLC_IP=10.11.99.1
 VLC_PASWD=changeme
-cvlc -I http --http-password $VLC_PASWD --http-host $VLC_IP --http-port $VLC_PORT --sout-keep --sout='#duplicate{dst=rtp{mux=ts,dst=10.11.99.1:8086},dst=gather:std{access=http,mux=mpeg1,dst=:8085},dst=display,select="novideo"}' -LZ /home/chip/content/** &
+cvlc -I http --http-password $VLC_PASWD --http-host $VLC_IP --http-port $VLC_PORT --sout-keep --sout='#duplicate{dst=rtp{mux=ts,dst=10.11.99.1:8086},dst=gather:std{access=http,mux=mpeg1,dst=:8085},dst=display,select="novideo"}' -LZ /home/chip/content/playlist.mp3 &
