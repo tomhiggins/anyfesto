88c5f3a77631b8d84d5be234907b366b1f3c9c67
Tom Higgins <tomhiggins@gmail.com>
2017-07-17 01:25:27 -0600

    Update start.sh

--- deployables/current/start.sh
+++ deployables/current/start.sh
@@ -3,6 +3,6 @@ VLC_PORT=8088
 VLC_USER=vlc
 VLC_IP=10.11.99.1
 VLC_PASWD=changeme
-find /home/chip/content/Radio -name '*.mp3' >/home/chip/content/playlist.m3u
+find /home/chip/content/Radio -name '*.mp3'>/home/chip/content/playlist.m3u
 chmod a+rwx /home/chip/content/playlist.m3u
 cvlc -I http --http-password $VLC_PASWD --http-host $VLC_IP --http-port $VLC_PORT --sout-keep --sout='#duplicate{dst=rtp{mux=ts,dst=10.11.99.1:8086},dst=gather:std{access=http,mux=mpeg1,dst=:8085},dst=display,select="novideo"}' -LZ /home/chip/content/playlist.mp3 &
