64afc025714805c479a3dcd5bca970bdc0298355
Tom Higgins <tomhiggins@gmail.com>
2020-07-16 22:41:21 -0500

    Gab the IP for use in M3U
    will need to make a variant for AnyfesTOR

--- deployables/current/makem3u.sh
+++ deployables/current/makem3u.sh
@@ -1,5 +1,6 @@
 rm /var/www/html/stream.m3u
-addy="http://ChangeToIpOrOnoin/content/Radio/"
+ip="$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')" 
+addy="http://$ip/content/Radio/"
 for file in /home/pi/content/Radio/*mp3
 do echo ""$addy"$(basename "$file")">>/var/www/html/stream.m3u
 done
