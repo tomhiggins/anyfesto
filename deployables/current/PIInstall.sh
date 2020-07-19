1ee1b046ba744ad6e4e59fd1326736059d0adde9
Tom Higgins <tomhiggins@gmail.com>
2020-07-18 22:50:08 -0500

    Update PIInstall.sh

--- deployables/current/PIInstall.sh
+++ deployables/current/PIInstall.sh
@@ -107,8 +107,8 @@ echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
 echo "Setting Up Kiwix Wikimedia Server "
 echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
 cd ~
-wget https://download.kiwix.org/bin/kiwix-server-arm.tar.bz2
-tar xvf kiwix-server-arm.tar.bz2
+wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf.tar.gz
+tar xvf kiwix-tools_linux-armhf.tar.gz
 chmod a+rx kiwix*
 rm kiwi*bz2
 sudo mv kiwix-serve /usr/bin/kiwix-serve
