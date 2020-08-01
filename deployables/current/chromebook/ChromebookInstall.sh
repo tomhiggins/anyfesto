3621bace6e9eed3a3260912e301a691e22ebaefa
tomwsmf <tomhiggins@gmail.com>
2020-07-31 21:26:21 -0500

    update kiwix

--- deployables/current/chromebook/ChromebookInstall.sh
+++ deployables/current/chromebook/ChromebookInstall.sh
@@ -106,12 +106,12 @@ echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
 echo "Setting Up Kiwix Wikimedia Server "
 echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
 cd ~
-wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-x86_64.tar.gz
-tar xvf kiwix-tools_linux-x86_64.tar.gz
+wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-x86_64-3.1.2-1.tar.gz
+tar xvf kiwix-tools_linux-x86_64*gz
 chmod a+rx kiwix*
-rm kiwi*bz2
-sudo mv kiwix-serve /usr/bin/kiwix-serve
-sudo mv kiwix-manage /usr/bin/kiwix-manage
+rm kiwi*z
+sudo mv kiwix*/kiwix-serve /usr/bin/kiwix-serve
+sudo mv kiwix*/kiwix-manage /usr/bin/kiwix-manage
 mkdir /home/anyfesto/content/kiwix
 cd /home/anyfesto/content/kiwix
 sudo wget http://download.kiwix.org/zim/wiktionary_en_simple_all.zim
