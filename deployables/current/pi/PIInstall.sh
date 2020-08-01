9a93f91a6649b4b3f9d479e544c526b5e4a24abc
tomwsmf <tomhiggins@gmail.com>
2020-07-31 21:15:48 -0500

    typos

--- deployables/current/pi/PIInstall.sh
+++ deployables/current/pi/PIInstall.sh
@@ -110,7 +110,7 @@ cd ~
 wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf.tar.gz
 tar xvf kiwix-tools_linux-armhf.tar.gz
 chmod a+rx kiwix*
-rm kiwi*bz2
+rm kiwi*z
 sudo mv kiwix*/kiwix-serve /usr/bin/kiwix-serve
 sudo mv kiwix*/kiwix-manage /usr/bin/kiwix-manage
 mkdir /home/pi/content/kiwix
