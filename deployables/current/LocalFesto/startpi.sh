4711168387a6791b049e66163add2890cfa07d31
Tom Higgins <tomhiggins@gmail.com>
2020-11-15 00:41:47 -0600

    Fix DST Duplication in VLC script

--- deployables/current/LocalFesto/startpi.sh
+++ deployables/current/LocalFesto/startpi.sh
@@ -5,4 +5,4 @@ VLC_IP="$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 
 VLC_PASWD=changeme
 find /home/pi/content/Radio -name '*.mp3'>/home/pi/content/playlist.m3u
 chmod a+rwx /home/pi/content/playlist.m3u
-cvlc -I http --http-password $VLC_PASWD --http-host $VLC_IP --http-port $VLC_PORT --sout-keep --sout='#duplicate{dst=rtp{mux=ts,dst=10.11.99.1:8086},dst=gather:std{access=http,mux=mpeg1,dst=:8085},dst=display,select="novideo"}' -LZ /home/pi/content/playlist.m3u &
+cvlc -I http --http-password $VLC_PASWD --http-host $VLC_IP --http-port $VLC_PORT --sout-keep --sout='#duplicate{dst=gather:std{access=http,mux=mpeg1,dst=:8085},dst=display,select="novideo"}' -LZ /home/pi/content/playlist.m3u &
